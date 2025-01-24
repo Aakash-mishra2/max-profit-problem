426d1818d944f8d59937803a0fd39b444c832b9a
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2025-01-24 18:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,78 @@
 function calculateMaxProfit(timeUnit) {
-    const buildTime = { T: 5, P: 4, C: 10 };
+    const buildTime = { T: 10, P: 4, C: 10 };
 
-    const earningsPerUnit = { T: 1500, P: 1000, C: 3000 };
+    const earningsPerUnit = { T: 3000, P: 1000, C: 3000 };
 
     //memoize earnigns and building combination for each time unit
     let data = new Array(timeUnit + 1).fill(null).map(() => ({
         earnings: 0,
-        combinations: {}
+        combinations: []
     }));
 
     for (let i = 4; i <= timeUnit; i++) {   //minimum time units to build any property = 4
         for (let buildType in buildTime) {
+
             if (i >= buildTime[buildType]) {
                 const currentEarning = (timeUnit - i) * earningsPerUnit[buildType]; //profit by current building hereafter
 
-                const temp = i - buildTime[buildType];
+                const temp = i - buildTime[buildType]; //0
                 const earnings = data[temp].earnings + currentEarning; //profit by previous buildings till i units
 
-                if (currentEarning && earnings > data[i].earnings) {
-                    data[i].earnings = earnings;
-                    data[i].combinations = {
-                        ...data[temp].combinations,
-                        [buildType]: (data[temp].combinations[buildType] || 0) + 1  // increment building units
-                    };
+
+                if (currentEarning && earnings >= data[i].earnings) { //add equal earnings edge case
+                    if (earnings > data[i].earnings) { // New maximum earnings, replace combinations
+                        data[i].earnings = earnings;
+                        data[i].combinations = data[temp].combinations.map(comb => ({
+                            ...comb,
+                            [buildType]: (comb[buildType] || 0) + 1
+                        }));
+
+                        // Ensure at least one combination exists for this earning
+                        if (data[temp].combinations.length === 0) {
+                            data[i].combinations = [{ [buildType]: 1 }];
+                        }
+                    }
+
+                    else if (earnings === data[i].earnings) {  // Add new combinations for equal earnings
+                        const newCombinations = data[temp].combinations.map((comb) => ({
+                            ...comb,
+                            [buildType]: (comb[buildType] || 0) + 1
+                        }));
+                        if (newCombinations.length === 0) {
+                            newCombinations.push({ [buildType]: 1 });
+                        }
+                        data[i].combinations.push(...newCombinations);
+                    }
                 }
             }
         }
+
+        //If no combinations exist, initialize with an empty one
+        if (data[i].combinations.length === 0) data[i].combinations.push({});
+
+        // Deduplicate combinations
+        data[i].combinations = data[i].combinations.filter(
+            (comb, index, self) =>
+                index === self.findIndex(other => JSON.stringify(other) === JSON.stringify(comb))   //stringify to compare objects in js
+        );
+
     }
 
-    data = data.sort((a, b) => a.earnings - b.earnings).reverse();
+
+    data = data.sort((a, b) => b.earnings - a.earnings);
+
     let i = 0;
-    const topCombinations = []; // holds max profit ( data[0].earnings ) combinations
+    let topCombinations = []  // holds all max profit ( data[0].earnings ) combinations
     while (i < data.length && data[i].earnings === data[0].earnings) {
-        topCombinations.push({
-            T: 0,
-            P: 0,
-            C: 0,
-            ...data[i].combinations
-        });
+        let tempCombinations = data[i].combinations.map(combination => ({
+            T: combination.T || 0,
+            P: combination.P || 0,
+            C: combination.C || 0
+        }))
+        topCombinations.push(...tempCombinations);
         i += 1;
     }
+
     return {
         earnings: data[0].earnings,
         combinations: topCombinations
